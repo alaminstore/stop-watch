0d262490c043ac2fedbf128f3184df2ebdef8e3e
Sm Al-amin <67338203+alaminstore@users.noreply.github.com>
2020-08-04 04:13:07 +0600

    Update CountDown.js
    Set values those are comes from App.js

--- src/components/CountDown/CountDown.js
+++ src/components/CountDown/CountDown.js
@@ -4,9 +4,9 @@ import Digit from "./Digit/Digit";
 const countDown = (props) => {
   return (
     <div className="d-flex">
-      <Digit color="#3C79B6" value="30" />
-      <Digit color="#FB6D6C" value="06" />
-      <Digit color="#B7201E" value="03" />
+      <Digit color="#3C79B6" value={props.time.min}/>
+      <Digit color="#FB6D6C" value={props.time.sec} />
+      <Digit color="#B7201E" value={props.time.mili} />
     </div>
   );
 };
